31b156aa9c649b9fedcc92957c3d8191ecc64ff7
garyoliver11 <garyoliver2000@gmail.com>
2025-03-15 18:27:41 -0600

    avance

---
+++ src/main/java/com/example/EC3_QuispeCabrera/Ec3QuispeCabreraApplication.java
@@ -0,0 +1,13 @@
+package com.example.EC3_QuispeCabrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3QuispeCabreraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3QuispeCabreraApplication.class, args);
+	}
+
+}
